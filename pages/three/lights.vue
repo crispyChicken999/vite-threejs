33d5c9c7f72843fc80a40587e89234566d4c712a
chenxiaojun <1483091515@qq.com>
2023-05-19 11:47:10 +0800

    给前三种光源添加GUI控制器

--- pages/three/lights.vue
+++ pages/three/lights.vue
@@ -19,7 +19,7 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import  GUI from "lil-gui";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 const { $THREE: THREE } = useNuxtApp();
 useHead({
   title: "Lights",
@@ -31,7 +31,11 @@ useHead({
     },
   ],
 });
+
+// 当前选中的光源
 const selectedLight = ref("ambient");
+
+// 生成光源
 function generateLights() {
   const scene = new THREE.Scene();
   const canvas = document.querySelector("#lights");
@@ -54,7 +58,6 @@ function generateLights() {
   controls.update();
 
   const planeSize = 40;
-
   const loader = new THREE.TextureLoader();
   const texture = loader.load(
     "https://threejs.org/manual/examples/resources/images/checker.png"
@@ -65,6 +68,7 @@ function generateLights() {
   const repeats = planeSize / 2;
   texture.repeat.set(repeats, repeats);
 
+  // 平面
   const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
   const planeMat = new THREE.MeshPhongMaterial({
     map: texture,
@@ -74,6 +78,7 @@ function generateLights() {
   mesh1.rotation.x = Math.PI * -0.5;
   scene.add(mesh1);
 
+  // 立方体
   const cubeSize = 4;
   const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
   const cubeMat = new THREE.MeshPhongMaterial({ color: "#8AC" });
@@ -81,6 +86,7 @@ function generateLights() {
   mesh2.position.set(cubeSize + 1, cubeSize / 2, 0);
   scene.add(mesh2);
 
+  // 球体
   const sphereRadius = 3;
   const sphereWidthDivisions = 32;
   const sphereHeightDivisions = 16;
@@ -94,14 +100,41 @@ function generateLights() {
   mesh3.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
   scene.add(mesh3);
 
-  // 环境光
-  const color = 0xffffff;
+  // 光线的基本参数
   const intensity = 1;
+  const color = 0xffffff;
+
+  // 定义ColorGUIHelper类
+  class ColorGUIHelper {
+    constructor(object, prop) {
+      this.object = object;
+      this.prop = prop;
+    }
+    get value() {
+      return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+      this.object[this.prop].set(hexString);
+    }
+  }
+
+  // 环境光
   const ambientLight = new THREE.AmbientLight(color, intensity);
+  const ambientLightGUI = new GUI({
+    container: document.querySelector(".container"),
+  });
+  ambientLightGUI
+    .addColor(new ColorGUIHelper(ambientLight, "color"), "value")
+    .name("color");
+  ambientLightGUI.add(ambientLight, "intensity", 0, 2, 0.01);
   watchEffect(() => {
-    selectedLight.value === "ambient"
-      ? (ambientLight.visible = true)
-      : (ambientLight.visible = false);
+    if (selectedLight.value === "ambient") {
+      ambientLight.visible = true;
+      ambientLightGUI.domElement.style.display = "block";
+    } else {
+      ambientLight.visible = false;
+      ambientLightGUI.domElement.style.display = "none";
+    }
   });
   scene.add(ambientLight);
 
@@ -113,12 +146,25 @@ function generateLights() {
     groundColor,
     intensity
   );
-  watchEffect(() => {
-    selectedLight.value === "hemisphere"
-      ? (hemisphereLight.visible = true)
-      : (hemisphereLight.visible = false);
+  const hemisphereLightGUI = new GUI({
+    container: document.querySelector(".container"),
   });
-
+  hemisphereLightGUI
+    .addColor(new ColorGUIHelper(hemisphereLight, "color"), "value")
+    .name("skyColor");
+  hemisphereLightGUI
+    .addColor(new ColorGUIHelper(hemisphereLight, "groundColor"), "value")
+    .name("groundColor");
+  hemisphereLightGUI.add(hemisphereLight, "intensity", 0, 2, 0.01);
+  watchEffect(() => {
+    if (selectedLight.value === "hemisphere") {
+      hemisphereLight.visible = true;
+      hemisphereLightGUI.domElement.style.display = "block";
+    } else {
+      hemisphereLight.visible = false;
+      hemisphereLightGUI.domElement.style.display = "none";
+    }
+  });
   scene.add(hemisphereLight);
 
   // 方向光
@@ -130,15 +176,23 @@ function generateLights() {
   scene.add(light.target);
   const helper = new THREE.DirectionalLightHelper(light);
   scene.add(helper);
+  const gui = new GUI({ container: document.querySelector(".container") });
+  gui.addColor(new ColorGUIHelper(light, "color"), "value").name("color");
+  gui.add(light, "intensity", 0, 2, 0.01);
+  gui.add(light.target.position, "x", -10, 10);
+  gui.add(light.target.position, "z", -10, 10);
+  gui.add(light.target.position, "y", 0, 10);
   watchEffect(() => {
     if (selectedLight.value === "directional") {
       light.visible = true;
       light.target.visible = true;
       helper.visible = true;
+      gui.domElement.style.display = "block";
     } else {
       light.visible = false;
       light.target.visible = false;
       helper.visible = false;
+      gui.domElement.style.display = "none";
     }
   });
 
@@ -179,6 +233,7 @@ function generateLights() {
     }
   });
 
+  // 渲染
   function render(time) {
     time *= 0.001;
     renderer.render(scene, camera);
@@ -196,5 +251,12 @@ onMounted(() => {
 .controls,
 .container {
   margin-top: 20px;
+  position: relative;
+  width: fit-content;
+  ::v-deep().lil-gui {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
 }
 </style>
